fbe73f43a8c5e784f2ab7b6f9c9f82e926eb38cf
xvmingyuan <625587553@qq.com>
2019-08-06 17:21:19 +0800

    Initial commit

---
+++ order/src/main/java/com/xmy/controller/OrderHandler.java
@@ -0,0 +1,22 @@
+package com.xmy.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class OrderHandler {
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${eureka.client.serviceUrl.defaultZone}")
+    private String defaultZone;
+
+    @GetMapping("/index")
+    public String index() {
+        return this.port + "-" + this.defaultZone;
+    }
+}
+
